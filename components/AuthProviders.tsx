ee390d4e6df4246515b413918844bbb0a8e300c3
gift56 <efeasiughu@gmail.com>
2023-08-29 14:21:41 -0700

    done connecting grafbase

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,7 +1,18 @@
 "use client";
 
-import { getProviders, signIn } from 'next-auth/react';
+import { getProviders, signIn } from "next-auth/react";
 import { useState, useEffect } from "react";
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | undefined;
+};
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
   return <div>AuthProviders</div>;
