f92306c83b164ae5700ecba9bc0fe18295c353cd
gift56 <efeasiughu@gmail.com>
2023-08-29 16:33:36 -0700

    working on connections

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,5 +1,10 @@
 import { GraphQLClient } from "graphql-request";
 
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const client = new GraphQLClient("apiUrl");
+
 const makeGraphQLRequest = async (query: string, variables = {}) => {
   try {
   } catch (error) {
