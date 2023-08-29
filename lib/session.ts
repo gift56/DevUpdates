e72d36d900958466a032a05826fbccb7e8b6f23f
gift56 <efeasiughu@gmail.com>
2023-08-29 14:43:05 -0700

    done connecting grafbase

--- lib/session.ts
+++ lib/session.ts
@@ -4,6 +4,7 @@ import { AdapterUser } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
 import jsonwebtoken from "jsonwebtoken";
 import { JWT } from "next-auth/jwt";
+import { SessionInterface, UserProfile } from "@/common.types";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -34,4 +35,52 @@ export const authOptions: NextAuthOptions = {
     colorScheme: "light",
     logo: "/img/logo.svg",
   },
+  callbacks: {
+    async session({ session }) {
+      const email = session?.user?.email as string;
+
+      try {
+        const data = (await getUser(email)) as { user?: UserProfile };
+
+        const newSession = {
+          ...session,
+          user: {
+            ...session.user,
+            ...data?.user,
+          },
+        };
+
+        return newSession;
+      } catch (error: any) {
+        console.error("Error retrieving user data: ", error.message);
+        return session;
+      }
+    },
+    async signIn({ user }: { user: AdapterUser | User }) {
+      try {
+        const userExists = (await getUser(user?.email as string)) as {
+          user?: UserProfile;
+        };
+
+        if (!userExists.user) {
+          await createUser(
+            user.name as string,
+            user.email as string,
+            user.image as string
+          );
+        }
+
+        return true;
+      } catch (error: any) {
+        console.log("Error checking if user exists: ", error.message);
+        return false;
+      }
+    },
+  },
 };
+
+export async function getCurrentUser() {
+  const session = (await getServerSession(authOptions)) as SessionInterface;
+
+  return session;
+}
