216707db59de4e1ecf146a4397398da7f1ca5450
gift56 <efeasiughu@gmail.com>
2023-08-29 10:03:22 -0700

    working on some stuff

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Dev Updates",
+  description:
+    "Showcase and discover remarkable developer projects and Tutorial guides",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
