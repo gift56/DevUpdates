683ae7adb2b18b1224ee44e17816925f4cc0acbb
gift56 <efeasiughu@gmail.com>
2023-08-29 10:08:56 -0700

    working on some stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import { Metadata } from "next";
 import "./globals.css";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Dev Updates",
   description:
     "Showcase and discover remarkable developer projects and Tutorial guides",
