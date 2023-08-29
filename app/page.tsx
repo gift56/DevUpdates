45bc8f2d102f75cf3dc834ef35cbb32da64e7e08
gift56 <efeasiughu@gmail.com>
2023-08-29 10:33:55 -0700

    done defining classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Homepage = () => {
-  return <section className="flexStart paddings mb-16"></section>;
+  return (
+    <section className="flexStart paddings mb-16">
+      <h1>Catergory</h1>
+      <h1>Post</h1>
+      <h1>Loadmore</h1>
+    </section>
+  );
 };
 
 export default Homepage;
