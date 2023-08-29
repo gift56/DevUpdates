0da19634452508fbbcccedf4c0724f214d57966a
gift56 <efeasiughu@gmail.com>
2023-08-29 11:09:28 -0700

    working on project real time

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
@@ -5,7 +6,12 @@ const Navbar = () => {
     <nav className="flexBetween navbar">
       <div className="flex-1 flexStart gap-10">
         <Link href="/">
-          
+          <Image
+            src="/img/logo.svg"
+            alt="Dev_updates"
+            width={116}
+            height={43}
+          />
         </Link>
       </div>
     </nav>
