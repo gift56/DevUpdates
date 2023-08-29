49b38f897ccc2ed63d6bb2ad3e7b7aec6c6684da
gift56 <efeasiughu@gmail.com>
2023-08-29 11:25:53 -0700

    working on project real time

--- components/index.tsx
+++ components/index.tsx
@@ -1,5 +1,6 @@
+import AuthProviders from "./AuthProviders";
 import Button from "./Button";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-export { Navbar, Footer, Button };
+export { Navbar, Footer, Button, AuthProviders };
