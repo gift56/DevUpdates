91d4cc33384dbaff62ac5cea0c66d80b14aebd90
gift56 <efeasiughu@gmail.com>
2023-08-29 16:28:50 -0700

    working on connections

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,3 +1,8 @@
 import { GraphQLClient } from "graphql-request";
 
-const makeGraphQLRequest = async (query: string, variables = {}) => {};
+const makeGraphQLRequest = async (query: string, variables = {}) => {
+  try {
+  } catch (error) {
+    throw error;
+  }
+};
