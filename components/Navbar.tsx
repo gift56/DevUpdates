db5e67bdb4705764f405bfbb7303c3e82f54de85
gift56 <efeasiughu@gmail.com>
2023-08-29 10:57:33 -0700

    working on project real time

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,15 @@
+import Link from "next/link";
+
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href="/">
+          
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
