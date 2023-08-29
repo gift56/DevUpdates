b70f6e70fce2a208f4b81573278b21d3a64460bb
gift56 <efeasiughu@gmail.com>
2023-08-29 11:30:09 -0700

    working on project real time

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,4 +1,25 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { footerLinks } from "@/constant";
+
+type ColumnProps = {
+  title: string;
+  links: Array<string>;
+};
+
+const FooterColumn = ({ title, links }: ColumnProps) => (
+  <div className="footer_column">
+    <h4 className="font-semibold">{title}</h4>
+    <ul className="flex flex-col gap-2 font-normal">
+      {links.map((link) => (
+        <Link href="/" key={link}>
+          {link}
+        </Link>
+      ))}
+    </ul>
+  </div>
+);
 
 const AuthProviders = () => {
   return <div>AuthProviders</div>;
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return <footer className="flexStart footer">Footer</footer>;
 };
 
 export default Footer;
