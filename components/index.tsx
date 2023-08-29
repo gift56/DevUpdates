86ba130cd5cfedb4a6610e1d688edaf33ab05ed0
gift56 <efeasiughu@gmail.com>
2023-08-29 13:10:00 -0700

    working on graphbase

--- components/index.tsx
+++ components/index.tsx
@@ -1,6 +1,7 @@
 import AuthProviders from "./AuthProviders";
 import Button from "./Button";
+import Categories from "./Categories";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-export { Navbar, Footer, Button, AuthProviders };
+export { Navbar, Footer, Button, AuthProviders, Categories };
