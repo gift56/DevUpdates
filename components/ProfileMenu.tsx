6feda503f6c06156a81a3901f1f25bf32207218f
gift56 <efeasiughu@gmail.com>
2023-08-30 07:23:46 -0700

    working on connections

--- components/ProfileMenu.tsx
+++ components/ProfileMenu.tsx
@@ -1,3 +1,10 @@
+"use client"
+
+import Link from "next/link";
+import Image from "next/image";
+import { signOut } from "next-auth/react";
+import { Fragment, useState } from "react";
+
 import { SessionInterface } from "@/common.types";
 
 const ProfileMenu = ({ session }: { session: SessionInterface }) => {
