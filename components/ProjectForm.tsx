5c069d555570e337b0a3e1641878e82af305349e
gift56 <efeasiughu@gmail.com>
2023-08-30 17:13:35 -0700

    working on new changes

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,4 +1,13 @@
-import React from "react";
+"use client"
+
+import { ProjectInterface, SessionInterface } from "@/common.types";
+
+
+type ProjectProps = {
+  type: string,
+  session: SessionInterface,
+  project?: ProjectInterface
+}
 
 const ProjectForm = () => {
   return <div>ProjectForm</div>;
