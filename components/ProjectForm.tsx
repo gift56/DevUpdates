4599dda91b3be2f88f89e3e0b046bb63e5a21143
gift56 <efeasiughu@gmail.com>
2023-08-30 17:16:00 -0700

    working on creating a project

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,16 +1,21 @@
-"use client"
+"use client";
 
 import { ProjectInterface, SessionInterface } from "@/common.types";
-
+import { FormEvent } from "react";
 
 type ProjectProps = {
-  type: string,
-  session: SessionInterface,
-  project?: ProjectInterface
-}
+  type: string;
+  session: SessionInterface;
+  project?: ProjectInterface;
+};
 
-const ProjectForm = () => {
-  return <div>ProjectForm</div>;
+const ProjectForm = ({ type, session, project }: ProjectProps) => {
+
+  const handleFormSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+  };
+  
+  return <form onSubmit={handleFormSubmit} className="flexStart form"></form>;
 };
 
 export default ProjectForm;
