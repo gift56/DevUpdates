fa28eb44c585442ac77f796159acbb39188470a3
gift56 <efeasiughu@gmail.com>
2023-08-30 18:08:10 -0700

    working on creating a project

--- components/CustomMenu.tsx
+++ components/CustomMenu.tsx
@@ -1,13 +1,18 @@
-"use client"
+"use client";
 
-import { Fragment } from 'react';
-import { Menu, Transition } from '@headlessui/react';
-import Image from 'next/image';
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import Image from "next/image";
+
+type Props = {
+  title: string;
+  state: string;
+  filters: Array<string>;
+  setState: (value: string) => void;
+};
 
 const CustomMenu = () => {
-  return (
-    <div>CustomMenu</div>
-  )
-}
+  return <div>CustomMenu</div>;
+};
 
-export default CustomMenu
+export default CustomMenu;
