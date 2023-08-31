2c21128711a44db40c237e8913a64af33a8247ec
gift56 <efeasiughu@gmail.com>
2023-08-30 17:20:06 -0700

    working on creating a project

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { ProjectInterface, SessionInterface } from "@/common.types";
-import { FormEvent } from "react";
+import { FormState, ProjectInterface, SessionInterface } from "@/common.types";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+import Image from 'next/image';
 
 type ProjectProps = {
   type: string;
@@ -10,12 +12,75 @@ type ProjectProps = {
 };
 
 const ProjectForm = ({ type, session, project }: ProjectProps) => {
+  const router = useRouter();
+
+  const [submitting, setSubmitting] = useState<boolean>(false);
+  const [form, setForm] = useState<FormState>({
+    title: project?.title || "",
+    description: project?.description || "",
+    image: project?.image || "",
+    liveSiteUrl: project?.liveSiteUrl || "",
+    githubUrl: project?.githubUrl || "",
+    category: project?.category || "",
+  });
+
+  const handleStateChange = (fieldName: keyof FormState, value: string) => {
+    setForm((prevForm) => ({ ...prevForm, [fieldName]: value }));
+  };
+
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    const file = e.target.files?.[0];
+
+    if (!file) return;
+
+    if (!file.type.includes("image")) {
+      alert("Please upload an image!");
+
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      const result = reader.result as string;
+
+      handleStateChange("image", result);
+    };
+  };
 
   const handleFormSubmit = async (e: FormEvent) => {
     e.preventDefault();
   };
-  
-  return <form onSubmit={handleFormSubmit} className="flexStart form"></form>;
+
+  return (
+    <form onSubmit={handleFormSubmit} className="flexStart form">
+      <div className="flexStart form_image-container">
+        <label htmlFor="poster" className="flexCenter form_image-label">
+          {!form.image && "Choose a poster for your project"}
+        </label>
+        <input
+          id="image"
+          type="file"
+          accept="image/*"
+          required={type === "create" ? true : false}
+          className="form_image-input"
+          onChange={(e) => handleChangeImage(e)}
+        />
+        {form.image && (
+          <Image
+            src={form?.image}
+            className="sm:p-10 object-contain z-20"
+            alt="image"
+            fill
+          />
+        )}
+      </div>
+    </form>
+  );
 };
 
 export default ProjectForm;
